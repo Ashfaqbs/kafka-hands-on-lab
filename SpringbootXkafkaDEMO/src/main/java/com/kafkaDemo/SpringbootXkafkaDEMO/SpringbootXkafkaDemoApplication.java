1c1d109c7bb27394e80eb4c95ba738e7405e93af
Ashfaq <105435085+DarkSharkAsh@users.noreply.github.com>
2023-10-08 18:44:13 +0530

    C1 project 2
    This project is about demonstration of using already created kafka topic which is used /created from terminal i.e  
    E:\kafka>.\bin\windows\kafka-topics.bat --create --topic myFirstTopic --bootstrap-server localhost:9092
    Created topic myFirstTopic.
    
    E:\kafka>.\bin\windows\kafka-console-producer.bat --topic myFirstTopic --bootstrap-server localhost:9092
    
    and using this topic in kafkatemplate producer message and  @kafkalistener consumer

---
+++ SpringbootXkafkaDEMO/src/main/java/com/kafkaDemo/SpringbootXkafkaDEMO/SpringbootXkafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaDemo.SpringbootXkafkaDEMO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootXkafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootXkafkaDemoApplication.class, args);
+	}
+
+}
---
+++ SpringbootXkafkaDEMO/src/test/java/com/kafkaDemo/SpringbootXkafkaDEMO/SpringbootXkafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaDemo.SpringbootXkafkaDEMO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootXkafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
