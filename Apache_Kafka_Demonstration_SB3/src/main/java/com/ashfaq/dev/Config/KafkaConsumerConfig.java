15cf0d83852db8e219e2496b5905e56eea99ccd9
Ashfaqbs <ash001x@gmail.com>
2024-04-26 12:04:01 +0530

    good practice and logs data

--- Apache_Kafka_Demonstration_SB3/src/main/java/com/ashfaq/dev/Config/KafkaConsumerConfig.java
+++ Apache_Kafka_Demonstration_SB3/src/main/java/com/ashfaq/dev/Config/KafkaConsumerConfig.java
@@ -15,26 +15,47 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 @Configuration
 public class KafkaConsumerConfig {
 
-//    @Value("${spring.kafka.consumer.bootstrap-servers}")
-//    private String bootstrapServers;
+
+	@Value("${spring.kafka.consumer.bootstrap-servers}")
+	private String bootstrapServers;
+
+	@Value("${spring.kafka.consumer.key-deserializer}")
+	private String keySerializer;
+
+	@Value("${spring.kafka.consumer.value-deserializer}")
+	private String valueSerializer;
+
+	@Value("${spring.kafka.consumer.group-id}")
+	private String groupid;
+
+//	    @Value("${kafka.security.protocol}")
+//	    private String securityProtocol;
 //
-//    @Value("${spring.kafka.consumer.group-id}")
-//    private String groupId;
+//	    @Value("${kafka.sasl.mechanism}")
+//	    private String saslMechanism;
+//
+//	    @Value("${kafka.sasl.jaas.config}")
+//	    private String saslJaasConfig;
 
-    @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
-        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        return new DefaultKafkaConsumerFactory<>(configProps);
-    }
+	@Bean
+	public ConsumerFactory<String, String> consumerFactory() {
+		Map<String, Object> configProps = new HashMap<>();
+		configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		configProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupid);
+		configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keySerializer);
+		configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueSerializer);
 
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        return factory;
-    }
+//      configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "kafka.security.protocol=SASL_SSL");
+//      configProps.put(SaslConfigs.SASL_MECHANISM, "kafka.sasl.mechanism=PLAIN");
+//      configProps.put(SaslConfigs.SASL_JAAS_CONFIG, "kafka.sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username='your_username' password='your_password';
+
+		return new DefaultKafkaConsumerFactory<>(configProps);
+	}
+
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory());
+		return factory;
+	}
 }
--- Apache_Kafka_Demonstration_SB3/src/main/java/com/ashfaq/dev/Config/KafkaProducerConfig.java
+++ Apache_Kafka_Demonstration_SB3/src/main/java/com/ashfaq/dev/Config/KafkaProducerConfig.java
@@ -15,26 +15,41 @@ import org.springframework.kafka.core.ProducerFactory;
 @Configuration
 public class KafkaProducerConfig {
 
-//    @Value("${spring.kafka.producer.bootstrap-servers}")
-//    private String bootstrapServers;
+	@Value("${spring.kafka.producer.bootstrap-servers}")
+	private String bootstrapServers;
 
-    @Bean
-    public ProducerFactory<String, String> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        
+	@Value("${spring.kafka.producer.key-serializer}")
+	private String keySerializer;
+
+	@Value("${spring.kafka.producer.value-serializer}")
+	private String valueSerializer;
+
+//	    @Value("${kafka.security.protocol}")
+//	    private String securityProtocol;
+//
+//	    @Value("${kafka.sasl.mechanism}")
+//	    private String saslMechanism;
+//
+//	    @Value("${kafka.sasl.jaas.config}")
+//	    private String saslJaasConfig;
+
+	@Bean
+	public ProducerFactory<String, String> producerFactory() {
+		Map<String, Object> configProps = new HashMap<>();
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+
 //        configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "kafka.security.protocol=SASL_SSL");
 //        configProps.put(SaslConfigs.SASL_MECHANISM, "kafka.sasl.mechanism=PLAIN");
-//        configProps.put(SaslConfigs.SASL_JAAS_CONFIG, "kafka.sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username="your_username" password="your_password";
+//        configProps.put(SaslConfigs.SASL_JAAS_CONFIG, "kafka.sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username='your_username' password='your_password';
 //);
-        
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
 
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
+		return new DefaultKafkaProducerFactory<>(configProps);
+	}
+
+	@Bean
+	public KafkaTemplate<String, String> kafkaTemplate() {
+		return new KafkaTemplate<>(producerFactory());
+	}
 }
