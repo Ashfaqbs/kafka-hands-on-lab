7a0eea80b582c1428df791b9fe740598424d9def
Ashfaq <105435085+Ashfaqbs@users.noreply.github.com>
2024-05-04 18:44:57 +0530

    Consumer config  update
    Update KafkaConsumerConfig.java

--- SB3-03-MultipleBroker/src/main/java/com/ashfaq/dev/Config/KafkaConsumerConfig.java
+++ SB3-03-MultipleBroker/src/main/java/com/ashfaq/dev/Config/KafkaConsumerConfig.java
@@ -43,14 +43,32 @@ public class KafkaConsumerConfig {
 		configProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupid);
 		configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keySerializer);
 		configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueSerializer);
+		// Set the consumer to start from the earliest offset
+                configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
 
 		configProps.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 30000);
 		configProps.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 60000);
 		configProps.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 50);
 
-//      configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "kafka.security.protocol=SASL_SSL");
-//      configProps.put(SaslConfigs.SASL_MECHANISM, "kafka.sasl.mechanism=PLAIN");
-//      configProps.put(SaslConfigs.SASL_JAAS_CONFIG, "kafka.sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username='your_username' password='your_password';
+
+//      configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL"); for consumer this is fine , as AdminClientConfig is for more acess 
+// configProps.put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, "SASL_SSL
+		
+ //       Admin Client Configurations: These are specifically designed for administrative tasks, allowing for operations such as creating, deleting, 
+// 	and managing topics, consumers, and producers within a Kafka cluster. The admin client configurations are focused on administrative
+// 	tasks and include settings for SSL/TLS, authentication, and other security-related parameters necessary for administrative operations 4.
+ //        Common Client Configurations: These are broader configurations applicable to both producers and consumers, including settings for connection details, 
+	// serialization/deserialization, and consumer-specific settings like group ID, auto-offset-reset, and session timeout. While these configurations are
+	// not exclusive to consumers, they are essential for defining how a consumer interacts with the Kafka cluster, including how it reads data, handles offsets,
+	// and manages its connection to the cluster 2.
+		
+		
+//      configProps.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
+//      configProps.put(SaslConfigs.SASL_JAAS_CONFIG, "org.apache.kafka.common.security.plain.PlainLoginModule required username='your_username' password='your_password';");
+
+
+		
 
 		return new DefaultKafkaConsumerFactory<>(configProps);
 	}
